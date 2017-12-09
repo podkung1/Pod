a2fea990e1177a20d7c23c02d4820073b0e84080
Pod <Pod@hotmail.com>
2017-12-09 11:01:06 +0700

    test

---
+++ PODPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+//This is Add Library
+//นี่คือตัวอย่างการเพิ่ม Library
+import UIKit
+
+//การประกาศตัวแปร แบบที่สามารถแก้ไขค่าได้
+//นี่คือการประกาศตัวแปร แบบ String ที่ใช้ค่าในการกำหนด DataType
+var nameString = "Pod"
+var numberInt = 123
+var numberDouble = 12.34
+var statusBol = true
+
+//Try Change Value
+nameString = "Doremon"
+numberInt = 321
+
+
