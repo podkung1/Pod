56995d5abdf406e3682fca0742ad040290e68e74
Pod <Pod@hotmail.com>
2017-12-09 11:34:58 +0700

    Select DataType

--- PODPlayground.playground/Contents.swift
+++ PODPlayground.playground/Contents.swift
@@ -15,7 +15,18 @@ var statusBol = true
 nameString = "Doremon"
 numberInt = 321
 
+
 //การประกาศตัวแปรที่ไม่สามารถแก้ไขค่าได้ หรือค่าคงที่
 let conNameString = "Nopita"
 
 //conNameString = "Doremon" ไม่สามารถทำได้ เพราะเป็น let
+
+//การประกาศตัวแปรแบบกำหนด Data Type
+var surname: String = "Rattanaphibundej"
+var myNumber: Int = 321
+var myManyNumber: Double = 32.123
+var myStatus: Bool = false
+
+
+
+
