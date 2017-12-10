a10b42f7eac1310f5787481d80b73a61017a85bc
Pod <Pod@hotmail.com>
2017-12-10 11:47:23 +0700

    เรียนรู้เรื่อง Optional

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Explicit Variable to String Datatype
+var numberString: String = "six"
+
+//Change Data Type String ==> Integer
+var numberInt = Int(numberString)
+
+//var answerInt = numberInt! + 10
+
+//วิีีธีการแก้
+if let myNumberInt = numberInt {
+    
+    var answerInt = myNumberInt + 10
+    
+} // if
