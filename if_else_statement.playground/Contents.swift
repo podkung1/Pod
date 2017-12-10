b8435f04ed6c590d7350cf946e2754f845996dc5
Pod <Pod@hotmail.com>
2017-12-10 10:35:02 +0700

    อ้างอิง array

---
+++ if_else_statement.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let ageInt = 40
+
+if (ageInt<18) {
+//    True
+    print("You are Young")
+} else {
+//    False
+    print("You are Old")
+}
+
+let nameString = "Doremon"
+let friendStrings = ["Doremon","Nobita"]
+
+if (nameString == friendStrings[1]) {
+    print("Mt Friend is \(nameString)")
+} else {
+    print("Not My Friend")
+}
+
+		
