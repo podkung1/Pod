aa0bc851b6233efc2ef87f1bbb34698e33378720
Pod <Pod@hotmail.com>
2017-12-10 11:02:48 +0700

    for loop Statement

---
+++ ForLoop_Statement.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let constantInts = [11,22,33,44,55]
+var timesInt = 0
+for myContantInts in constantInts {
+    timesInt += 1
+    print("timeInt ==> \(timesInt) value of myCon ==> \(myContantInts)")
+}
+
+
+
